28520de85968c83a2ef10929d6ace18a019597f1
jinhyeok6941 <66781994+jinhyeok6941@users.noreply.github.com>
2023-09-11 21:46:44 +0900

    Tetris
    2023-09-11 Commit

---
+++ Assets/Scripts/TetrisBlock.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class TetrisBlock : MonoBehaviour
+{
+    public Transform cubeContent;
+    public Transform [] cubes;
+
+    public Transform previewEmptyContent;
+    public Transform [] previewEmpties;  
+}
